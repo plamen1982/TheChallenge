5fb5b0731cc6c49058cfc57fa1ffaf82d7a8d6b4
plamen1982 <plamen.hristov@ymail.com>
2019-06-01 14:46:23 +0300

    US0002-Fetch-Data-From-Fake-API-Done

--- client-side/screens/ExploreScreen.js
+++ client-side/screens/ExploreScreen.js
@@ -1,24 +1,58 @@
 import React from 'react';
 import {
-  ScrollView,
+  ActivityIndicator,
   StyleSheet,
-  Text,
   View,
+  FlatList,
+  Text
 } from 'react-native';
 
-export default class ExploreScreen extends React.Component {
-  render() {
-    return (
+import UserService  from '../services/user-service'
+export default class ExploreScreen extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      dataSource:[]
+      };
+    }
+
+    static userService = new UserService();
+
+    _keyExtractor = (item) => item.id.toString();
+
+    render(){
+      if(this.state.loading){
+       return( 
+        <View style={styles.loader}> 
+          <ActivityIndicator size="large" color="#0c9"/>
+        </View>
+     )}
+
+     return(
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Text>Explore Screen</Text>
-          </View>
-        </ScrollView>
-      </View>
-    );
+        <Text>Fun Section</Text>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.name}</Text>}
+          keyExtractor={this._keyExtractor}
+        />
+     </View>
+     )}
+
+    async componentDidMount() {
+      try {
+        const dataSource = await ExploreScreen.userService.getAllUsers();
+        this.setState({
+          dataSource,
+          loading: false
+        });
+      } catch(error) {
+        console.log(error);
+      }
+    }
   }
-}
 
 const styles = StyleSheet.create({
   container: {
